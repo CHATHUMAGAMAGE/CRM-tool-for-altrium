8d0954243ca7283a9c19e591e496fe988f0518a3
scredy <ndsellahennadi2004@gmail.com>
2026-07-25 22:29:50 +0530

    feat: add JWT authentication and user roles

---
+++ backend/accounts/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+# Create your views here.
+from rest_framework.generics import RetrieveAPIView
+from rest_framework.permissions import IsAuthenticated
+
+from .serializers import CurrentUserSerializer
+
+
+class CurrentUserView(RetrieveAPIView):
+    serializer_class = CurrentUserSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_object(self):
+        return self.request.user
